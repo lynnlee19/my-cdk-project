21646e4d3274023bb703f84d972e02c1a3e5e421
unknown <jameilee@yfp.local>
2025-12-02 08:37:57 +0800

    first commit

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ MyCdkProjectStack(app, "MyCdkProjectStack",
     # want to deploy the stack to. */
 
     #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
+    env=cdk.Environment(account='752953536465', region='ap-northeast-1'),
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
 
--- my_cdk_project/my_cdk_project_stack.py
+++ my_cdk_project/my_cdk_project_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_s3 as s3,   
 )
 from constructs import Construct
 
@@ -17,3 +18,19 @@ class MyCdkProjectStack(Stack):
         #     self, "MyCdkProjectQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        MyL1Bucket = s3.CfnBucket(    #  創建一個L1的s3 Bucket construct實例
+            self,
+            "MyL1Bucket",       #  這是construct id，輸出yaml中的construct名字
+            bucket_name="mmy-30daysaws-example-l1bucket",  # s3 bucket真正的名字(全球唯一)
+            versioning_configuration={ #  物件被更新時不刪除而是分配新的版本ID
+                "status": "Enabled"
+            }
+        )
+
+        MyL2Bucket = s3.Bucket(
+            self,
+            "MyL2Bucket",
+            bucket_name="mmy-30daysaws-example-l2bucket",
+            versioned=True,
+        )        
